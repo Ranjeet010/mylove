6ef9729aa46bedc24d4bf4b9f2e9579a51145eda
Ranjeet Kumar <109428865+Ranjeet010@users.noreply.github.com>
2025-06-14 19:36:15 +0530

    Update scrips.js

--- scrips.js
+++ scrips.js
@@ -5,7 +5,7 @@ const yesBtn = document.getElementById("yes-btn");
 const noBtn = document.getElementById("no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "I knew it 😍";
+  question.innerHTML = "I knew it that you Love me 😍";
   gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
 
